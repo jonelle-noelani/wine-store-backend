c7e4393b77ad486491f1d212c03433b16c7b43d9
jonelle-noelani <jonelle.noelani@gmail.com>
2020-11-25 16:40:42 -0800

    minor syntax changes in users/auth controllers

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,10 +2,12 @@ class Api::V1::AuthController < ApplicationController
     skip_before_action :authorized, only [:create]
 
     def create
+        debugger
         user = User.find_by(email: user_login_params[:email])
         if user && user.authenticate(user_login_params[:password])
             token = encode({user_id: user.id})
             render json: { user: UserSerializer.new(user), jwt: token }, status: :accepted
+            
         else
             render json: { message: 'Invalid username or password'}, status: :unauthorized
         end
@@ -14,7 +16,7 @@ class Api::V1::AuthController < ApplicationController
     private
 
     def user_login_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:email, :password)
     end
-    
+
 end
